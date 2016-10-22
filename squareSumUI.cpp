c6fc1f3e4399f4f7352896130bb78000a19e7fc7
Pavel Antonov <p.antonov@list.ru>
2016-10-22 21:25:08 +0300

    Использование QVector даёт прирости в скорости, но у QVector int в качестве ключа, соответственно, когда происходит переполнение int, вектор падает. Использование std::vector обходит это. но из std::vector нельзя перейти в QSet. Использование std::set убивает своим find весь прирост скорости за счёт вектора

--- squareSumUI.cpp
+++ squareSumUI.cpp
@@ -36,7 +36,7 @@ void squareSumUI::on_findSquares_clicked()
     m_squareSumsHash.clear();
 
     //Делаем проверку на переполнение qint64 в строке ввода и на то, что ввели текст.
-    if(inText!= "0" && m_inNumber == 0)
+    if(inText != "0" && m_inNumber == 0)
     {
         ui->logListWidget->insertItem(0,"Введён текст или число превыщающее INT64_MAX");
     }
@@ -73,8 +73,8 @@ bool squareSumUI::generateSequence()
     qint64 sum = number;
     qint64 border = 9223312036854775807; //уменьшенный INT64_MAX, чтобы ввести границу, когда начинаешь проверять на переполнение.
     m_squaresSet.clear();
-    m_squaresSet.insert(sum);
-    do{
+    while(sum <= m_inNumber){
+        m_squaresSet.insert(sum);
         number+=2;
         if(sum >= border)
         {
@@ -85,10 +85,9 @@ bool squareSumUI::generateSequence()
             }
         }
         sum += number;
-        m_squaresSet.insert(sum);
-
-    }while(sum < m_inNumber);
+    }
     ui->logListWidget->insertItem(0,"generation time=" + QString::number(start.elapsed()) + " ms");
+    ui->logListWidget->insertItem(0,"setSize=" + QString::number(m_squaresSet.size()) + " ms");
     return true;
 }
 
@@ -109,6 +108,133 @@ bool squareSumUI::findSumSquares(qint64 n_inputNumber)
     {
         dif = n_inputNumber - entryInSet;
         if(m_squaresSet.contains(dif))
+        {
+            m_squareSumsHash.insert(entryInSet,dif);
+        }
+    }
+    ui->logListWidget->insertItem(0,"find time="+QString::number(start.elapsed())+" ms");
+
+    return true;
+}
+
+void squareSumUI::on_findSquaresVector_clicked()
+{
+    ui->logListWidget->clear();
+    QString inText = ui->leIn->text();
+    m_inNumber = ui->leIn->text().toULongLong();
+    m_squaresSet.clear();
+    m_squareSumsHash.clear();
+
+    //Делаем проверку на переполнение qint64 в строке ввода и на то, что ввели текст.
+    if(inText != "0" && m_inNumber == 0)
+    {
+        ui->logListWidget->insertItem(0,"Введён текст или число превыщающее INT64_MAX");
+    }
+    else
+    {
+        this->generateSequenceStdVector();
+        this->findSumSquaresStd(m_inNumber);
+
+        // TODO переделать на замену значений в таблице
+        QHash<qint64, qint64>::const_iterator iter = m_squareSumsHash.constBegin();
+        QHash<qint64,qint64>::const_iterator stop = m_squareSumsHash.constEnd();
+        while (iter != stop) {
+            ui->logListWidget->insertItem(0,QString::number(iter.key())+" "+QString::number(iter.value()));
+            ++iter;
+        }
+    }
+}
+
+/**
+ * @brief squareSumUI::generateSequence - генерирует последовательность чисел с шагом 2, начиная с 1.
+ * @details Из школьного курса алгебры известно, что квадрат любого натурального числа может быть
+ * представлен, как сумма N натуральных чисел с шагом два, где N - натуральное число, чей квадрат мы ищем.
+ * Пример:
+ * Дана последовательность: 1, 3, 5, 7. 2^2 = 1+3 = 4; 3^2 = 1 + 3 + 5 = 4 + 5 = 9.
+ *
+ * При этом, операции сложения выполняются быстрее, чем операции умножения/возведения в степень, т.к. они скрывают в себе множественные операции сложения.
+ * @param n_count - сколько элементов должно быть в последовательности.
+ * @return true - если прошло без существенных для нас ошибок. (Переполнение стека тут не учитывается).
+ */
+
+//Это даёт прирости в скорости, но у QVector int в качестве ключа, соответственно, когда происходит переполнение int, вектор падает.
+//Использование std::vector обходит это. но из std::vector нельзя перейти в QSet. Использование std::set убивает своим find весь прирост скорости за счёт вектора
+bool squareSumUI::generateSequenceVector()
+{
+    QTime start = QTime::currentTime();
+    qint64 number = 1;
+    qint64 sum = number;
+    qint64 border = 9223312036854775807; //уменьшенный INT64_MAX, чтобы ввести границу, когда начинаешь проверять на переполнение.
+    m_squaresVector.clear();
+    while(sum <= m_inNumber)
+    {
+        m_squaresVector.append(sum);
+        number+=2;
+        if(sum >= border)
+        {
+            //Проверка на переполнение qint64
+            if(INT64_MAX-sum < number)
+            {
+                break;
+            }
+        }
+        sum += number;
+    };
+    ui->logListWidget->insertItem(0,"vectorSize=" + QString::number(m_squaresVector.size()) + " ms");
+    m_squaresSet = m_squaresVector.toList().toSet();
+    m_squaresVector.clear();
+    ui->logListWidget->insertItem(0,"generation time=" + QString::number(start.elapsed()) + " ms");
+
+    return true;
+}
+
+bool squareSumUI::generateSequenceStdVector()
+{
+    QTime start = QTime::currentTime();
+    qint64 number = 1;
+    qint64 sum = number;
+    qint64 border = 9223312036854775807; //уменьшенный INT64_MAX, чтобы ввести границу, когда начинаешь проверять на переполнение.
+    m_squaresStdVector.clear();
+    while(sum <= m_inNumber)
+    {
+        m_squaresStdVector.push_back(sum);
+        number+=2;
+        if(sum >= border)
+        {
+            //Проверка на переполнение qint64
+            if(INT64_MAX-sum < number)
+            {
+                break;
+            }
+        }
+        sum += number;
+    };
+    ui->logListWidget->insertItem(0,"vectorSize=" + QString::number(m_squaresStdVector.size()) + " ms");
+    std::set<qint64> squaresStdSet(m_squaresStdVector.begin(),m_squaresStdVector.end());
+    m_squaresStdSet = squaresStdSet;
+    m_squaresStdVector.clear();
+    ui->logListWidget->insertItem(0,"generation time=" + QString::number(start.elapsed()) + " ms");
+
+    return true;
+}
+
+bool squareSumUI::findSumSquaresStd(qint64 n_inputNumber)
+{
+    QTime start = QTime::currentTime();
+    m_squareSumsHash.clear();
+    if(m_squaresStdSet.find(n_inputNumber) != m_squaresStdSet.end())
+    {
+        m_squareSumsHash.insert(0, n_inputNumber);
+    }
+
+    // Тесты показали, что проверка на наличие повторов не даёт заметного прироста скорости в диапазоне qint64.
+    //связано это с тем, что таких пар не так много, и число лишних проверок превышает число пар.
+    qint64 dif;
+    qint64 entryInSet;
+    foreach(entryInSet, m_squaresStdSet)
+    {
+        dif = n_inputNumber - entryInSet;
+        if(m_squaresStdSet.find(dif) != m_squaresStdSet.end())
         {
             m_squareSumsHash.insert(entryInSet,dif);
         }
--- squareSumUI.h
+++ squareSumUI.h
@@ -5,6 +5,8 @@
 #include <QSet>
 #include <QTime>
 #include <QFile>
+#include <QDebug>
+#include <set>
 
 namespace Ui {
 class squareSumUI;
@@ -21,9 +23,14 @@ public:
 private slots:
     void on_findSquares_clicked();
 
+    void on_findSquaresVector_clicked();
+
 private:
     Ui::squareSumUI *ui;
     QSet<qint64> m_squaresSet; /** @brief Используется для хранения квадратов чисел.*/
+    std::set<qint64> m_squaresStdSet;
+    QVector<qint64> m_squaresVector; /** @brief Используется для хранения квадратов чисел.*/
+    std::vector<qint64> m_squaresStdVector;
     QHash<qint64, qint64> m_squareSumsHash;
     qint64 m_inNumber;
 
@@ -32,7 +39,11 @@ private:
 
 //    Для теста скорости чтения и закрузки из файла
 //    bool saveSequenceToFile();
-//    bool loadSequence();
+    //    bool loadSequence();
+    bool generateSequenceVector();
+    //bool findSumSquaresVector(qint64 n_inputNumber);
+    bool generateSequenceStdVector();
+    bool findSumSquaresStd(qint64 n_inputNumber);
 };
 
 #endif // SQUARESUMUI_H
